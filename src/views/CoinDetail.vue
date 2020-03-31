313a817f87c1984e1895a273be48ec0557955443
Erickowski <daniel_perez_03081995@hotmail.com>
2020-03-30 19:36:48 -0600

    Loader y Grafica

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="flex-col">
-        <template v-if="asset.id">
+        <div class="flex justify-center">
+            <bounce-loader :loading="isLoading" :color="'#68D391'" :size="100" />
+        </div>
+        <template v-if="!isLoading">
             <div class="flex flex-col sm:flex-row justify-around items-center">
                 <div class="flex flex-col items-center">
                     <img
@@ -53,6 +56,13 @@
                     <span class="text-xl"></span>
                 </div>
             </div>
+            <line-chart
+                class="my-10"
+                :colors="['orange']"
+                :min="min"
+                :max="max"
+                :data="history.map(h => [h.date, parseFloat(h.priceUsd).toFixed(2)])"
+            />
         </template>
     </div>
 </template>
@@ -64,6 +74,7 @@ export default {
     name: 'CoinDetail',
     data() {
         return {
+            isLoading: false,
             asset: {},
             history: []
         }
@@ -91,12 +102,15 @@ export default {
     methods: {
         getCoin() {
             const id = this.$route.params.id;
-            Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(
-                ([asset, history]) => {
-                    this.asset = asset
-                    this.history = history
-                }
-            )
+            this.isLoading = true;
+            Promise.all([api.getAsset(id), api.getAssetHistory(id)])
+                .then(
+                    ([asset, history]) => {
+                        this.asset = asset
+                        this.history = history
+                    }
+                )
+                .finally(() => (this.isLoading = false))
         }
     }
 }
